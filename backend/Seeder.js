970a5041c3b8feb7f83826941621b66fcc89f0b5
sujitha S <sujiselvam18@gmail.com>
2026-06-10 16:53:33 +0530

    update api url to production

--- backend/Seeder.js
+++ backend/Seeder.js
@@ -6,13 +6,15 @@ const products = require('./data/products.json');
 
 dotenv.config({ path: path.join(__dirname, 'config', 'config.env') });
 
-mongoose.connect(process.env.DB_URL).then(async () => {
+mongoose.connect(process.env.DB_URL)
+  .then(async () => {
     console.log('Connected to MongoDB');
     await Product.deleteMany();
     await Product.insertMany(products);
-    console.log('✅ Products imported successfully!');
+    console.log('Products imported successfully!');
     process.exit();
-}).catch(err => {
+  })
+  .catch(err => {
     console.log('Error:', err);
     process.exit();
-});
+  });
